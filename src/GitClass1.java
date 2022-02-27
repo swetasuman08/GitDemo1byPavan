4cd8fcefdb32313d97d74e4f5588b31a61934205
ashish.kumar <ashish.kumar@D8WCQV2.appsassoc.com>
2022-02-27 14:23:51 -0500

    Added additional checks

--- src/GitClass1.java
+++ src/GitClass1.java
@@ -2,10 +2,16 @@
 public class GitClass1{
 
 	public static void main(String[] args) {
+		
+		int a = 2;
+		int b=5;
 		System.out.println("My first project");
 		for (int i=1; i<10;i++)
-       System.out.println(i);
-       
+			
+		{ 
+			System.out.println(i);
+		}
+        System.out.println(a+b);
 
 	}
 
