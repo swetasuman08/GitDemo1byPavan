f22e0827f918c9514017c32d43362b1b514777c7
ashish.kumar <ashish.kumar@D8WCQV2.appsassoc.com>
2022-02-27 13:54:33 -0500

    Committing GitClass1 to BranchX

---
+++ src/GitClass1.java
@@ -0,0 +1,12 @@
+
+public class GitClass1{
+
+	public static void main(String[] args) {
+		System.out.println("My first project");
+		for (int i=1; i<10;i++)
+       System.out.println(i);
+       
+
+	}
+
+}
